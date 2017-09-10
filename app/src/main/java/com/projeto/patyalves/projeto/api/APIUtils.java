00e533c2ec06216c62290d7b57e9810bebbd4a30
patyalves17 <patriciaalves10@gmail.com>
2017-09-10 09:55:36 -0300

    [PAA] carregaLocais

--- app/src/main/java/com/projeto/patyalves/projeto/api/APIUtils.java
+++ app/src/main/java/com/projeto/patyalves/projeto/api/APIUtils.java
@@ -8,7 +8,7 @@ public class APIUtils {
     private APIUtils() {}
     public static final String BASE_URL = "http://www.mocky.io";
     public static final String BASE_URL_servidor = "http://192.168.1.113:8084/api/";
- 
+
 
     public static UsersAPI getUserAPI() {
         return RetrofitClient.getClient(BASE_URL).create(UsersAPI.class);
