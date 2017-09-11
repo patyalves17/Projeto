d2037ec912b76fca98f619fa9ed7154623252834
patyalves17 <patriciaalves10@gmail.com>
2017-09-10 22:11:30 -0300

    [PAA]  details fragment flatbutton

--- app/src/main/java/com/projeto/patyalves/projeto/PlaceDetailFragment.java
+++ app/src/main/java/com/projeto/patyalves/projeto/PlaceDetailFragment.java
@@ -21,6 +21,7 @@ import com.projeto.patyalves.projeto.model.Local;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -35,7 +36,11 @@ public class PlaceDetailFragment extends Fragment {
     private LocalsAPI localAPI;
     Long id;
 
-     @BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.fab1) FloatingActionButton fab1;
+    @BindView(R.id.fab2) FloatingActionButton fab2;
+    private Boolean isFabOpen = false;
+    private Animation fab_open,fab_close,rotate_forward,rotate_backward;
 
 
     public PlaceDetailFragment() {
@@ -47,14 +52,22 @@ public class PlaceDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_place_detail,container,false);
+        ButterKnife.bind(this,view);
         Bundle bundle=getArguments();
          id = bundle.getLong("localId",0);
         Log.i("carregaLocais", "Fragment 2 --> "+id.toString());
         loadDetails();
 
         Log.d("Raj", "Fab try");
+        fab_open = AnimationUtils.loadAnimation(getContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getContext(),R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getContext(),R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getContext(),R.anim.rotate_backward);
 
-        return inflater.inflate(R.layout.fragment_place_detail, container, false);
+
+        return view;
+      //  return inflater.inflate(R.layout.fragment_place_detail, container, false);
     }
 
 //    @OnClick(R.id.fab)
@@ -66,6 +79,15 @@ public class PlaceDetailFragment extends Fragment {
     @OnClick(R.id.fab)
     public void click(View view){
         Log.i("carregaLocais", "clicou no fab");
+        animateFAB();
+    }
+    @OnClick(R.id.fab1)
+    public void click1(View view){
+        Log.i("carregaLocais", "clicou no fab 1");
+    }
+    @OnClick(R.id.fab2)
+    public void click2(View view){
+        Log.i("carregaLocais", "clicou no fab 2");
     }
 
     private void loadDetails(){
@@ -79,7 +101,6 @@ public class PlaceDetailFragment extends Fragment {
                     Log.i("carregaLocais", response.body().toString());
                     Log.i("carregaLocais",response.body().getName());
                     Log.i("carregaLocais",response.body().getBairro());
-
                 }
             }
 
@@ -90,6 +111,29 @@ public class PlaceDetailFragment extends Fragment {
         });
     }
 
+    public void animateFAB(){
 
+        if(isFabOpen){
+
+            fab.startAnimation(rotate_backward);
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            isFabOpen = false;
+            Log.d("Raj", "close");
+
+        } else {
+
+            fab.startAnimation(rotate_forward);
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            isFabOpen = true;
+            Log.d("Raj","open");
+
+        }
+    }
 
 }
